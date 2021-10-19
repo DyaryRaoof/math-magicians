68b7ee427bbd39d40246da28680e069959f4f589
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-10-19 20:04:06 +0300

    Refactor App component to use classes instead

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return <Calculator />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
